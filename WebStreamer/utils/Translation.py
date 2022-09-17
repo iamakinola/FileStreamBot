c3e53ce3a16482ccfe497b4f32e9bd939455cf24
olaniyi akinola <38849125+iamakinola@users.noreply.github.com>
2022-09-17 13:14:40 +0100

    Update Translation.py

--- WebStreamer/utils/Translation.py
+++ WebStreamer/utils/Translation.py
@@ -7,23 +7,18 @@ class Language(object):
     class en(object):
         START_TEXT = """
 <i>👋 Hᴇʏ,</i>{}\n
-<i>I'm File Stream Bot As Well as Direct Links Generator</i>\n
+<i>I'm Telegram Files Streaming Bot As Well Direct Links Generator</i>\n
 <i>Cʟɪᴄᴋ ᴏɴ Hᴇʟᴘ ᴛᴏ ɢᴇᴛ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</i>\n
 <i><u>𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸</u></i>\n
-<b>🔞 SENDING PRONOGRAPHIC CONTENTS TO BOT MAY LEADS TO PERMANENT BAN.🔞</b>\n
+<b>🔞 Pʀᴏɴ ᴄᴏɴᴛᴇɴᴛꜱ ʟᴇᴀᴅꜱ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ ʙᴀɴ ʏᴏᴜ.</b>\n\n"""
 
         HELP_TEXT = """
-<i>- SEND ME A FILE (OR)FORWARED MEDIA FROM TELEGRAM.</i>
-<i>- I WILL PROVIVE DIRECT DOWNLOAD LINK AND WATCH LINK !.</i>
-<i>- IF THE FILE IS PLAYABLE, THE WATCH LINK WILL STREAM THE FILE.</i>
-<i>- LINKS GENERATED DO NOT EXPIRE AND IT CAN BE EMBED TO WEBSITES</i>
-<i>- LINKS GENERATED CAN BE STREAM</i>
-<i>- DOWNLOAD LINK WITHFASTEST SPEED</i>
-<i>- FILE RESOLUTIONS AND SIZE REMAIN THE SAME>
-<i>MY LINKS DO NOT EXPIRE AND IT CAN BE EMBED TO WEBSITES</i>\n
-<u>🚸𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸</u>\n
-<b>🔞 SENDING PRONOGRAPHIC CONTENTS TO BOT MAY LEADS TO PERMANENT BAN.🔞</b>\n
-<i>CONTACT DEVELOPER (OR) REPORT BUG</i> <b>:@iamakinola</b>"""
+<i>- Sᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ (ᴏʀ) ᴍᴇᴅɪᴀ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ.</i>
+<i>- I ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴇxᴛᴇʀɴᴀʟ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ !.</i>
+<i>- ᴅᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ Wɪᴛʜ Fᴀsᴛᴇsᴛ Sᴘᴇᴇᴅ</i>
+<u>🔸 𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸</u>\n
+<b>🔞 Pʀᴏɴ ᴄᴏɴᴛᴇɴᴛꜱ ʟᴇᴀᴅꜱ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ ʙᴀɴ ʏᴏᴜ.</b>\n
+<i>Cᴏɴᴛᴀᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ (ᴏʀ) ʀᴇᴘᴏʀᴛ ʙᴜɢꜱ</i> <b>: <a href='https://t.me/{}'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>"""
 
         ABOUT_TEXT = """
 <b>⚜ Mʏ ɴᴀᴍᴇ : Public Link Generator</b>\n
@@ -32,7 +27,6 @@ class Language(object):
 """
 
         stream_msg_text ="""
-<b>💰FOR DONATION CONTACT DEVELOPER @iamakinola💰</b>\n
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
