bab717a599983fc1a16099da2ea651603cf383fb
olaniyi akinola <38849125+iamakinola@users.noreply.github.com>
2022-09-17 15:28:08 +0100

    Update Translation.py

--- WebStreamer/utils/Translation.py
+++ WebStreamer/utils/Translation.py
@@ -8,49 +8,51 @@ class Language(object):
         START_TEXT = """
 <i>👋 Hᴇʏ,</i>{}\n
 <i>I am <b>AutoFileStreamBot</b> As Well As Direct Links Generator. Here i what i do.</i>\n
-<i>1, Send me any file (or) forwarded file from telegram</i>
-<i>2, I will generate direct download link and watch link</i>
+<i>1, Send me any file (or) forwarded file from telegram.</i>
+<i>2, I will generate direct download link and watch link.</i>
 <i>If the file is playable, the watch link will stream the file (.mp4 required)</i>
 <i>3, This link generated do not expire. </i>
-<i>4, Download link with the fastest speed</i>\n
-<i>CLICK ON HELP BUTTON TO GET MORE INFORMATIONS</i>\n
-<i><u>⚠️𝗪𝗔𝗥𝗡𝗜𝗡𝗚⚠️</u></i>
+<i>4, Download link with the fastest speed.</i>\n
+<i>CLICK ON <b>HELP</b> BUTTON TO GET MORE INFORMATIONS</i>\n
+<i><u>⚠️𝗪𝗔𝗥𝗡𝗜𝗡𝗚⚠️</i></u>
 <b>🔞SENDING PORNOGRAPHIC CONTENTS TO BOT MAY LEAD TO PERMANENT BAN🔞</b>\n
 <b>💰For SUPPORT AND DONATIONS💵</b>
-<i>please contact Developer: 👨‍💻@iamakinola</i>\n"""
+<i>Please contact Developer: 👨‍💻@iamakinola</i>\n"""
 
         HELP_TEXT = """
-<i>I am <b>AutoFileStreamBot</b> As Well As Direct Links Generator. Here i what i do.</i>\n
+<i><b>Helps</i></b>\n
 <i>1, Send me any file (or) forwarded file from telegram.</i>
 <i>Be it Videos, Pictures, Musics, Documents(pdf,txt), Games, Softwares,etc </i>
 <i>2, I will generate direct download link and watch link</i>
 <i>If the file is playable to watch, the watch link will stream the file (.mp4 required)</i>
 <i><b>Note: Watch link is generated for every file. But if file is not playable(mp4, mp3), watch link wont work.</i><b>
 <i>3, This link generated do not expire. </i>
-<i>4, Download link with the fastest speed</i>\n
-<i>5, Bot supported almost all format.
+<i>4, Download link with the fastest speed.</i>
+<i>5, Bot supported almost all format.</i>\n
 <i><u>⚠️𝗪𝗔𝗥𝗡𝗜𝗡𝗚⚠️</u></i>
 <b>🔞SENDING PORNOGRAPHIC CONTENTS TO BOT MAY LEAD TO PERMANENT BAN🔞</b>\n
 <b>Need this Bot for your personal project? <b>: <a href='https://t.me/iamakinola'>[ CONTACT US ]</a></b>
 <b>💰For SUPPORT AND DONATIONS💵</b>
 <i>CONTACT DEVELOPER (OR REPORT BUGS</i> <b>: <a href='https://t.me/iamakinola'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>\n
-<i>IS THIS BOT HELPFUL TOOL? </i>: <a href='https://t.me/iamakinola'>[LEAVE A REVIEW]</a></b>"""
+<i>IS THIS BOT HELPFUL TOOL? </i>  <b>: <a href='https://t.me/tlgrmcbot?start=autofilestreambot-review</a></b>"""
 
         ABOUT_TEXT = """
 <b>⚜ BOT NAME : AutoFileStream</b>\n
 <b>🔸VERSION : 3.0.3.1</b>\n
 <b>👨‍💻DEVELOPER : olaniyi akinola @iamakinola</b>\n
-<b>🔹Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ : [ 09-July-22 ] 12:36 AM</b>
+<b>🔹Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ : [ 09-July-21 ] 12:36 AM</b>
 <b>💰For SUPPORT AND DONATIONS💵</b>
 <i>CONTACT DEVELOPER (OR REPORT BUGS</i> <b>: <a href='https://t.me/iamakinola'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>\n
-<i>IS THIS BOT HELPFUL TOOL? </i>: <a href='https://t.me/iamakinola'>[LEAVE A REVIEW]</a></b>"""
+<i>IS THIS BOT HELPFUL TOOL? </i>  <b>: <a href='https://t.me/tlgrmcbot?start=autofilestreambot-review'>[LEAVE A REVIEW]</a></b>"""
 
         stream_msg_text ="""
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
 <b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n
-<b>🖥WATCH :</b> <i>{}</i>"""
+<b>🖥WATCH :</b> <i>{}</i>\n
+<i>CONTACT DEVELOPER (OR REPORT BUGS</i> <b>: <a href='https://t.me/iamakinola'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>\n
+<i>IS THIS BOT HELPFUL TOOL? </i>  <b>: <a href='https://t.me/tlgrmcbot?start=autofilestreambot-review'>[LEAVE A REVIEW]</a></b>"""
 
 
         ban_text="__Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ.__\n\n**[Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ](tg://user?id={}) Tʜᴇʏ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**"
